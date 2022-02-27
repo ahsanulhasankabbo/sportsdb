96d9980daa44089ecf70f21f85388de001a57cb5
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-02-28 01:28:01 +0600

    api faced

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
 const playerName = () =>{
+    document.getElementById('player-container').textContent = '';
     const searchFeild = document.getElementById("search-field") ;
     const searchValue = searchFeild.value;
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
@@ -13,21 +14,21 @@ const playerName = () =>{
 
 const displayPlayer = players => {
     const playerContainer = document.getElementById('player-container');
-    playerContainer.textContent = '';
     for(const player of players){
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card" style="width: 18rem;">
             <img src="${player.strThumb}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${player.strPlayer}</h5>
-                <p class="card-text">${player.strDescriptionEN.slice(0,200)}</p>
+                <h3 class="card-title">Name : ${player.strPlayer}</h3>
+                <h5 class="card-title">Country : ${player.strNationality}</h5>
+                <h5 class="card-title">Sports : ${player.strSport}</h5>
                 <button onclick="loadDetails('${player.idPlayer}')" class="btn bg-success text-white">Details</button>
             </div>
         </div>
         `
         playerContainer.appendChild(div);
-        // console.log(player)
+        console.log(player)
     }
 }
 
@@ -43,8 +44,7 @@ const displayDetails = info =>{
     const detailsContainer = document.getElementById('details-container');
     detailsContainer.innerHTML = `
     <img class="w-50" src="${info.players[0].strThumb}" alt="">
-    <h3>Name: ${info.players[0].strPlayer}</h3>
-    <h3>Country: ${info.players[0].strNationality}</h3>
-    <h3>Sport: ${info.players[0].strSport}</h3>
+    <h3 class="card-title">${info.players[0].strPlayer}</h3>
+    <p>Sport: ${info.players[0].strDescriptionEN}</p>
     `
 }
